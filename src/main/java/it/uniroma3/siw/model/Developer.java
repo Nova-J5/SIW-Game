4883bfc29496e382c237c66a1f98de87a26881e7
Nova <mce_asterisks@aleeas.com>
2023-04-16 12:28:38 +0200

    Aggiornate le classi Developer, Game, Genre e Platform con le annotazioni jakarta.validation.constraints per la gestione della validazione.

---
+++ src/main/java/it/uniroma3/siw/model/Developer.java
@@ -0,0 +1,107 @@
+package it.uniroma3.siw.model;
+
+import java.util.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+
+@Entity
+public class Developer {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	@NotBlank
+	private String name;
+	
+	@NotNull
+	@Max(2023)
+	private Integer yearOfFoundation;
+	
+	@Column(length = 2000)
+	private String description;
+	
+	@OneToMany(mappedBy = "developer")
+	private List<Game> gamesProduced;
+	
+	@OneToMany(mappedBy = "developer")
+	private List<Platform> platformsProduced;
+	
+	
+	/*************************************
+	 ********** GETTER E SETTER **********
+	 *************************************/
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getYearOfFoundation() {
+		return yearOfFoundation;
+	}
+
+	public void setYearOfFoundation(Integer yearOfFoundation) {
+		this.yearOfFoundation = yearOfFoundation;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Game> getGames() {
+		return gamesProduced;
+	}
+
+	public void setGames(List<Game> games) {
+		this.gamesProduced = games;
+	}
+
+	public List<Platform> getPlatformsProduced() {
+		return platformsProduced;
+	}
+
+	public void setPlatformsProduced(List<Platform> platformsProduced) {
+		this.platformsProduced = platformsProduced;
+	}
+
+	
+	/***************************************
+	 ********** EQUALS E HASHCODE **********
+	 ***************************************/
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id, yearOfFoundation);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Developer other = (Developer) obj;
+		return Objects.equals(Id, other.Id) && Objects.equals(yearOfFoundation, other.yearOfFoundation);
+	}
+	
+	
+}
---
+++ src/main/java/it/uniroma3/siw/model/Genre.java
@@ -0,0 +1,83 @@
+package it.uniroma3.siw.model;
+
+import java.util.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+
+@Entity
+public class Genre {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	@NotBlank
+	private String name;
+	
+	@Column(length = 2000)
+	private String description;
+	
+	@ManyToMany
+	private List<Game> games;
+	
+	
+	/*************************************
+	 ********** GETTER E SETTER **********
+	 *************************************/
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(List<Game> games) {
+		this.games = games;
+	}
+	
+	
+	/***************************************
+	 ********** EQUALS E HASHCODE **********
+	 ***************************************/
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Genre other = (Genre) obj;
+		return Objects.equals(Id, other.Id) && Objects.equals(name, other.name);
+	}
+	
+}

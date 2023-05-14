7f0d613586b79060959e59fe0a3ec651d42aedbb
massidale <massimo@dalessandro.it>
2023-05-14 18:40:03 +0200

    import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+--il valore nextval corrisponde all id
+insert into game (id, title, year, developer) values(nextval('hibernate_sequence'), 'Fortnite', 'Epic Games', 2017);
+insert into game (id, title, year, developer) values(nextval('hibernate_sequence'), 'Assassins Creed', 'Ubisoft', 2007);

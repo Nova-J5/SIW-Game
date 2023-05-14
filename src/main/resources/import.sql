17b8b0a1ec6242a4fb16c40318f15732230d3dbd
FlexBoi <diegob0349@gmail.com>
2023-05-14 19:02:06 +0200

    import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into game values (nextval('game_seq'), 'gioco bello', 'super mario', 2000);
+insert into game values (nextval('game_seq'), 'gioco brutto', 'pokemon scarlatto', 2022);
+insert into game values (nextval('game_seq'), 'gioco meraviglioso', 'zelda: breath of the wild', 2018);

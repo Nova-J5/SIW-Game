4ff3629351920b88ebbc6f1364e6c7d34b3e54c7
Nova <mce_asterisks@aleeas.com>
2023-07-04 19:00:53 +0200

    Aggiornato l'import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 insert into game values (nextval('game_seq'), 'gioco bello', 'super mario', 2000);
 insert into game values (nextval('game_seq'), 'gioco brutto', 'pokemon scarlatto', 2022);
 insert into game values (nextval('game_seq'), 'gioco meraviglioso', 'zelda', 2018);
-insert into genre values (nextval('genre_seq'), 'parentesi', 'azione');
-insert into platform values (nextval('platform_seq'), 'cazzopalle', 'nintendo', 1800)
+insert into genre values (nextval('genre_seq'), 'descrizione genere', 'azione');
+insert into platform values (nextval('platform_seq'), 'descrizione console', 'nintendo', 1800)
 insert into platform_games values (1, 1)
 insert into genre_games values (1, 1)
 

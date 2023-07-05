131d96c9d42ce032129289a91d5d02577f7f0023
Nova <mce_asterisks@aleeas.com>
2023-07-05 18:45:55 +0200

    Import con tutti i generi che ci interessano

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,73 @@
+
+
+
 insert into game values (nextval('game_seq'), 'gioco bello', 'super mario', 2000);
 insert into game values (nextval('game_seq'), 'gioco brutto', 'pokemon scarlatto', 2022);
 insert into game values (nextval('game_seq'), 'gioco meraviglioso', 'zelda', 2018);
-insert into genre values (nextval('genre_seq'), 'descrizione genere', 'azione');
-insert into platform values (nextval('platform_seq'), 'descrizione console', 'nintendo', 1800)
-insert into platform_games values (1, 1)
-insert into genre_games values (1, 1)
+insert into platform values (nextval('platform_seq'), 'descrizione console', 'nintendo', 1800);
 
+
+
+
+
+INSERT INTO developer VALUES (nextval('developer_seq'), 'descrizione', 'Nintendo', 1889);
+
+INSERT INTO developer VALUES (nextval('developer_seq'), 'descrizione', 'Sony', 1946);
+
+INSERT INTO developer VALUES (nextval('developer_seq'), 'descrizione', 'Microsoft', 1975);
+
+INSERT INTO developer VALUES (nextval('developer_seq'), 'descrizione', 'Team Cherry', 2014);
+
+
+
+
+INSERT INTO game VALUES (nextval('game_seq'), 'descrizione', 'Super Mario Odyssey', 2017);
+
+INSERT INTO game VALUES (nextval('game_seq'), 'descrizione', 'Pokémon Colosseum', 2003);
+
+INSERT INTO game VALUES (nextval('game_seq'), 'descrizione', 'Uncharted 4: Fine di un Ladro', 2016);
+
+INSERT INTO game VALUES (nextval('game_seq'), 'descrizione', 'Halo: Combat Evolved', 2001);
+
+INSERT INTO game VALUES (nextval('game_seq'), 'descrizione', 'Hollow Knight', 2017);
+
+
+
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Azione')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Arcade')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Avventura')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Beat em all')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Cooperazione')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Corse')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'FPS')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Gestionale')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Guerra')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Indie')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'MMO')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Piattaforme')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Picchiaduro')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'RPG')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Simulazione')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Single-player')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Sport')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'Strategia')
+
+INSERT INTO genre VALUES (nextval('genre_seq'), 'VR')

b4bdd708e76b088a19c0a32f1d5243ab790a3a9d
FlexBoi <diegob0349@gmail.com>
2023-07-06 18:58:20 +0200

    pagina games aggiornata

--- src/main/java/it/uniroma3/siw/controller/GameController.java
+++ src/main/java/it/uniroma3/siw/controller/GameController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,7 +52,7 @@ public class GameController {
 
 	@GetMapping("/games")
 	public String showGames(Model model) {
-		model.addAttribute("games", this.gameRepository.findAll());
+		model.addAttribute("genres", this.genreRepository.findAll());
 		return "games.html";
 	}
 	

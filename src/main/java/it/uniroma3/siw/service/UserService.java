71a73409c965816c01c86ff1039aefcfe2a13ca8
Nova <mce_asterisks@aleeas.com>
2023-07-08 11:31:53 +0200

    Aggiornata la pagina profilo dell'utente

--- src/main/java/it/uniroma3/siw/service/UserService.java
+++ src/main/java/it/uniroma3/siw/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.model.User;
 import it.uniroma3.siw.repository.UserRepository;
 
@@ -26,10 +27,12 @@ public class UserService {
 	public List<User> getAllUsers(){
 		return (List<User>) userRepository.findAll();
 	}
-
+	
+	@Transactional
 	public User findUserById(Long id) {
 		Optional<User> result = userRepository.findById(id);
 		return result.orElse(null);
 	}
 	
+
 }

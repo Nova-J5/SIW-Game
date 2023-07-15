51ddadc31aaf1163bb8458fdf71d7039041e8daf
Nova <mce_asterisks@aleeas.com>
2023-07-15 18:39:47 +0200

    È il momento gaming (sì) è il momento gaming (sì)

--- src/main/java/it/uniroma3/siw/controller/indexController.java
+++ src/main/java/it/uniroma3/siw/controller/indexController.java
@@ -8,21 +8,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import it.uniroma3.siw.model.Platform;
-import it.uniroma3.siw.repository.PlatformRepository;
+import it.uniroma3.siw.service.GenreService;
+import it.uniroma3.siw.service.PlatformService;
 
 @Controller
 public class indexController {
 
 	@Autowired
-	PlatformRepository platformRepository;
+	private PlatformService platformService;
+	
+	@Autowired
+	private GenreService genreService;
 	
 	@GetMapping("/")
 	public String index(Model model) {
-		List<Platform> allPlatforms = (List<Platform>) this.platformRepository.findAll();
+		List<Platform> allPlatforms = (List<Platform>) this.platformService.getAllPlatforms();
 		Platform platformActive = allPlatforms.get(0);
 		model.addAttribute("platformActive", platformActive);
 		allPlatforms.remove(platformActive);
 		model.addAttribute("platforms", allPlatforms);
+		model.addAttribute("genres", this.genreService.getAllGenres());
 		return "index.html";
 	}
 }
